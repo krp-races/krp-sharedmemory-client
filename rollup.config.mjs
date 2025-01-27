28da15fa68892e44ba3f537143222857c449e6f5
Fynn Haupt <info@fynnhaupt.de>
2025-01-27 12:19:31 +0100

    project setup

---
+++ rollup.config.mjs
@@ -0,0 +1,31 @@
+// rollup.config.mjs
+import typescript from '@rollup/plugin-typescript';
+import { globSync } from 'glob';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+export default {
+  input: Object.fromEntries(
+    globSync('src/**/!(*.test).ts').map((file) => [
+      path.relative('src', file.slice(0, file.length - path.extname(file).length)),
+      fileURLToPath(new URL(file, import.meta.url))
+    ])
+  ),
+  external: [],
+  output: [
+    {
+      dir: 'dist',
+      format: 'cjs',
+      exports: 'named',
+      strict: false
+    },
+    {
+      dir: 'dist',
+      format: 'es',
+      entryFileNames: '[name].mjs',
+      exports: 'named',
+      strict: false
+    }
+  ],
+  plugins: [typescript()]
+};
